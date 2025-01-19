c3de636015f37f3f6e4d876ad11ec13622357367
arazraw <araz.rawshani@gmail.com>
2025-01-19 02:08:10 +0100

    version 1

--- app.py
+++ app.py
@@ -20,35 +20,59 @@ st.write('''
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    # Create filters for the columns
-    hcm_in_title = st.selectbox('HCM in Title', options=['All', 'Yes', 'No'])
-    hcm_in_abstract = st.selectbox('HCM in Abstract', options=['All', 'Yes', 'No'])
-    ecg_in_title = st.selectbox('ECG in Title', options=['All', 'Yes', 'No'])
-    ecg_in_abstract = st.selectbox('ECG in Abstract', options=['All', 'Yes', 'No'])
+    st.subheader("HCM Filters")
+    hcm_title_col, hcm_title_select_col = st.columns([1, 3])
+    hcm_title_col.write("HCM in Title")
+    hcm_in_title = hcm_title_select_col.selectbox('', options=['Ignore', 'Yes', 'No'], key='hcm_title', help="Filter by HCM in Title", label_visibility="collapsed")
+
+    hcm_abstract_col, hcm_abstract_select_col = st.columns([1, 3])
+    hcm_abstract_col.write("HCM in Abstract")
+    hcm_in_abstract = hcm_abstract_select_col.selectbox('', options=['Ignore', 'Yes', 'No'], key='hcm_abstract', help="Filter by HCM in Abstract", label_visibility="collapsed")
+
+    st.subheader("ECG Filters")
+    ecg_title_col, ecg_title_select_col = st.columns([1, 3])
+    ecg_title_col.write("ECG in Title")
+    ecg_in_title = ecg_title_select_col.selectbox('', options=['Ignore', 'Yes', 'No'], key='ecg_title', help="Filter by ECG in Title", label_visibility="collapsed")
+
+    ecg_abstract_col, ecg_abstract_select_col = st.columns([1, 3])
+    ecg_abstract_col.write("ECG in Abstract")
+    ecg_in_abstract = ecg_abstract_select_col.selectbox('', options=['Ignore', 'Yes', 'No'], key='ecg_abstract', help="Filter by ECG in Abstract", label_visibility="collapsed")
 
     # Apply filters
     filtered_df = df.copy()
 
-    if hcm_in_title != 'All':
+    if hcm_in_title != 'Ignore':
         filtered_df = filtered_df[filtered_df['HCM_In_Title'] == hcm_in_title]
 
-    if hcm_in_abstract != 'All':
+    if hcm_in_abstract != 'Ignore':
         filtered_df = filtered_df[filtered_df['HCM_In_Abstract'] == hcm_in_abstract]
 
-    if ecg_in_title != 'All':
+    if ecg_in_title != 'Ignore':
         filtered_df = filtered_df[filtered_df['ECG_In_Title'] == ecg_in_title]
 
-    if ecg_in_abstract != 'All':
+    if ecg_in_abstract != 'Ignore':
         filtered_df = filtered_df[filtered_df['ECG_In_Abstract'] == ecg_in_abstract]
 
     # Display the number of papers in the resulting table
     st.write(f"Number of papers: {filtered_df.shape[0]}")
+
+    # Add a heading above the table
+    st.markdown("### Click for details")
 
     # Display the filtered DataFrame in the Streamlit app using AgGrid
     gb = GridOptionsBuilder.from_dataframe(filtered_df)
     gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
     gb.configure_side_bar()  # Add a sidebar
     gb.configure_selection('single')  # Enable single row selection
+
+    # Hide specific columns
+    gb.configure_columns(['HCM_In_Title', 'HCM_In_Abstract', 'ECG_In_Title', 'ECG_In_Abstract'], hide=True)
+
+    # Set column widths
+    gb.configure_column('PMID', width=200, flex=1)
+    gb.configure_column('TI', width=400, flex=2)
+    gb.configure_column('AB', width=400, flex=2)
+
     gridOptions = gb.build()
 
     grid_response = AgGrid(
